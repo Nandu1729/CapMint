466da58b6192683abce512071760ef78ca79d90d
Nandu1729 <nandu.ank3@gmail.com>
2026-07-26 13:27:29 +0530

    chore(repo): ignore and untrack node_modules and build artifacts

--- backend/auth-service/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'fastify' {
-    interface FastifyInstance {
-        authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
-        authorize: (allowedSpecs: any[]) => (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
-    }
-}
-export {};
