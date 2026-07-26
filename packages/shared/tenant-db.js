808a8f837d412973ad065e911df6c997c512cd02
Nandu1729 <nandu.ank3@gmail.com>
2026-07-26 14:23:49 +0530

    fix(shared): accept PostgreSQL UUID tenant identifiers

--- packages/shared/tenant-db.js
+++ packages/shared/tenant-db.js
@@ -1,5 +1,5 @@
 const UUID_PATTERN =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export const PUBLIC_TENANT_CONTEXT = Object.freeze({ access: 'public' });
 
